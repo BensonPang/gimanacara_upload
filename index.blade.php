68907daa235610fe78c074e1d90e7267736e26aa
BensonPang <122581374+BensonPang@users.noreply.github.com>
2023-03-21 22:14:08 +0700

    Add files via upload
    Assetnya gak bisa di upload pak maaf 
    tapi sudah saya bikin

---
+++ index.blade.php
@@ -0,0 +1,340 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>CV BENSON</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+  <!-- ======= Mobile nav toggle button ======= -->
+  <i class="bi bi-list mobile-nav-toggle d-xl-none"></i>
+
+  <!-- ======= Header ======= -->
+  <header id="header">
+    <div class="d-flex flex-column">
+
+      <div class="profile">
+        <img src="assets/img/profile-img.jpg" alt="" class="img-fluid rounded-circle" >
+        <h1 class="text-light"><a href="index.html">Benson Pang Tian Poh</a></h1>
+        <div class="social-links mt-3 text-center">
+          <a href="https://www.facebook.com/ben.benson.5454021" class="facebook"><i class="bx bxl-facebook"></i></a>
+          <a href="https://www.instagram.com/benson.ganteng/?hl=id" class="instagram"><i class="bx bxl-instagram"></i></a>
+          <a href="https://wa.me/628117772415" class="whatsapp"><i class="bx bxl-whatsapp"></i></a>
+          <a href="https://www.linkedin.com/in/benson-ganteng-4635b7157/" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+        </div>
+      </div>
+
+      <nav id="navbar" class="nav-menu navbar">
+        <ul>
+          <li><a href="#hero" class="nav-link scrollto active"><i class="bx bx-home"></i> <span>Home</span></a></li>
+          <li><a href="#about" class="nav-link scrollto"><i class="bx bx-user"></i> <span>About</span></a></li>
+          <li><a href="#resume" class="nav-link scrollto"><i class="bx bx-file-blank"></i> <span>Resume</span></a></li>
+          <li><a href="#contact" class="nav-link scrollto"><i class="bx bx-envelope"></i> <span>Contact</span></a></li>
+        </ul>
+      </nav><!-- .nav-menu -->
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="d-flex flex-column justify-content-center align-items-center">
+    <div class="hero-container" data-aos="fade-in">
+      <h1>Benson Pang Tian Poh</h1>
+      <p>I'm <span class="typed" data-typed-items="Designer, Developer, Freelancer, Accountant"></span></p>
+    </div>
+  </section><!-- End Hero -->
+
+  <main id="main">
+
+    <!-- ======= About Section ======= -->
+    <section id="about" class="about">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>About</h2>
+          <p>Benson adalah lulusan SMA Negeri 1 jurusan MIPA. Selama kuliah, Benson aktif dalam Organisasi Siswa Intra Sekolah (OSIS) sebagai anggota organisasi serta pernah menjuarai lomba Olimpiade Sains Nasional (OSN). Memiliki passion dalam Web Developer.</p>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-4" data-aos="fade-right">
+            <img src="assets/img/profile-img.jpg" class="img-fluid" alt="">
+          </div>
+          <div class="col-lg-8 pt-4 pt-lg-0 content" data-aos="fade-left">
+            <h3>UI/UX Designer &amp; Web Developer.</h3>
+            <div class="row">
+              <div class="col-lg-6">
+                <ul>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Birthday:</strong> <span>27 Januari 2004</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Phone:</strong> <span>+62 811 7772 415</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>City:</strong> <span>Kepulauan Riau, Batam</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Status:</strong> <span>Belum Menikah</span></li>
+                </ul>
+              </div>
+              <div class="col-lg-6">
+                <ul>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Age:</strong> <span>19</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Degree:</strong> <span>Sarjana</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Email:</strong> <span>Benson012705@gmail.com</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Freelance:</strong> <span>Available</span></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End About Section -->
+
+    
+    <!-- ======= Skills Section ======= -->
+    <section id="skills" class="skills section-bg">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>Skills</h2>
+        </div>
+
+        <div class="row skills-content">
+
+          <div class="col-lg-6" data-aos="fade-up">
+
+            <div class="progress">
+              <span class="skill">HTML <i class="val">100%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+            <div class="progress">
+              <span class="skill">CSS <i class="val">90%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+            <div class="progress">
+              <span class="skill">JavaScript <i class="val">75%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+          </div>
+
+          <div class="col-lg-6" data-aos="fade-up" data-aos-delay="100">
+
+            <div class="progress">
+              <span class="skill">PHP <i class="val">80%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+            <div class="progress">
+              <span class="skill">WordPress/CMS <i class="val">90%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+            <div class="progress">
+              <span class="skill">Photoshop <i class="val">55%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Skills Section -->
+
+    <!-- ======= Resume Section ======= -->
+    <section id="resume" class="resume">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>Resume</h2>
+          <p>Penjelasan singkat tentang pendidikan dan pengalaman kerja Benson.</p>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-6" data-aos="fade-up">
+            <h3 class="resume-title">Sumary</h3>
+            <div class="resume-item pb-0">
+              <h4>Benson Pang Tian Poh</h4>
+              <p><em>Highly creative and experienced graphic designer with more than 2 years of experience in managing design campaigns and projects by creating Facebook, social media conceptualization, and more. I am passionate about working in a digital marketing agency where I can be involved in a team to deliver the best result for clients in all fields.</em></p>
+              <ul>
+                <li>Jl. Sunaryo, Kemboja, Tanjungpinang Barat, Kemboja, Tanjungpinang Barat., Kota Tanjung Pinang, Kepulauan Riau 29111, Indonesia</li>
+                <li>(62) 771-23571</li>
+                <li>halobca@bca.co.id</li>
+              </ul>
+            </div>
+
+            <h3 class="resume-title">Education</h3>
+            <div class="resume-item">
+              <h4>Sarjana Studi Sistem Informasi &amp; Akutansi</h4>
+              <h5>2022</h5>
+              <p><em>Universitas Internasional Batam, Batam, Kepulauan Riau</em></p>
+            </div>
+            <div class="resume-item">
+              <h4>Matematika dan Ilmu Pengetahuan Alam</h4>
+              <h5>2018 - 2021</h5>
+              <p><em>SMA NEGERI 1 TANJUNGPINANG, Tanjungpinang, Kepulauan Riau</em></p>
+            </div>
+            <div class="resume-item">
+              <h4>Matematika dan Ilmu Pengetahuan Alam</h4>
+              <h5>2018 - 2021</h5>
+              <p><em>SMP NEGERI 1 TANJUNGPINANG, Tanjungpinang, Kepulauan Riau</em></p>
+            </div>
+            <div class="resume-item">
+              <h4></h4>
+              <h5>2018 - 2021</h5>
+              <p><em>SD NEGERI 013 TANJUNGPINANG, Tanjungpinang, Kepulauan Riau</em></p>
+            </div>
+          </div>
+          <div class="col-lg-6" data-aos="fade-up" data-aos-delay="100">
+            <h3 class="resume-title">Professional Experience</h3>
+            <div class="resume-item">
+              <h4>ShopeeAds Project Management</h4>
+              <h5>2021 - 2022</h5>
+              <ul>
+                <li>Mengimplementasikan strategi Ads yang berfokus pada popup mobile dan SEM.</li>
+                <li>Mengembangkan campaign iklan baru dengan membundling produk dan meningkatkan sales sebesar 17%. </li>
+                <li>Mengurangi anggaran campaign sebesar 35% dengan menggunakan cara yang lebih efektif.</li>
+              </ul>
+            </div>
+            <div class="resume-item">
+              <h4>Staff Akutansi</h4>
+              <h5>2022 - 2023</h5>
+              <ul>
+                <li>Melaksanakan pencatatan dan penjumlahan semua transaksi penjualan.</li>
+                <li>Berhasil mengidentifikasi masalah di perusahaan yang menghemat 2,5 juta rupiah per tahun.</li>
+                <li>Membantu menyiapkan dan menganalisa laporan keuangan secara akurat.</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End Resume Section -->
+
+    <!-- ======= Contact Section ======= -->
+    <section id="contact" class="contact">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>Contact</h2>
+          <p>Silahkan menghubungkan saya jika berkenan dengan alamat dibawah</p>
+        </div>
+
+        <div class="row" data-aos="fade-in">
+
+          <div class="col-lg-5 d-flex align-items-stretch">
+            <div class="info">
+              <div class="address">
+                <i class="bi bi-geo-alt"></i>
+                <h4>Location:</h4>
+                <p>Blok A3, Jl. Taman Kota Baloi No.06, Tj. Uma, Kec. Lubuk Baja, Kota Batam, Kepulauan Riau 29444</p>
+              </div>
+
+              <div class="email">
+                <i class="bi bi-envelope"></i>
+                <h4>Email:</h4>
+                <p>Benson012705@gmail.com</p>
+              </div>
+
+              <div class="phone">
+                <i class="bi bi-phone"></i>
+                <h4>Call:</h4>
+                <p>+62 811 7772 415</p>
+              </div>
+
+              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3989.0511217284725!2d104.00395309999999!3d1.1236264!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31d98baa3faa3269%3A0x4653198c7d9cc137!2sKos!5e0!3m2!1sid!2sid!4v1679380969535!5m2!1sid!2sid" frameborder="0" style="border:0; width: 100%; height: 290px;" allowfullscreen></iframe>
+            </div>
+
+          </div>
+
+          <div class="col-lg-7 mt-5 mt-lg-0 d-flex align-items-stretch">
+            <form action="forms/contact.php" method="post" role="form" class="php-email-form">
+              <div class="row">
+                <div class="form-group col-md-6">
+                  <label for="name">Your Name</label>
+                  <input type="text" name="name" class="form-control" id="name" required>
+                </div>
+                <div class="form-group col-md-6">
+                  <label for="name">Your Email</label>
+                  <input type="email" class="form-control" name="email" id="email" required>
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="name">Subject</label>
+                <input type="text" class="form-control" name="subject" id="subject" required>
+              </div>
+              <div class="form-group">
+                <label for="name">Message</label>
+                <textarea class="form-control" name="message" rows="10" required></textarea>
+              </div>
+              <div class="my-3">
+                <div class="loading">Loading</div>
+                <div class="error-message"></div>
+                <div class="sent-message">Your message has been sent. Thank you!</div>
+              </div>
+              <div class="text-center"><button type="submit">Send Message</button></div>
+            </form>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Contact Section -->
+
+  </main><!-- End #main -->
+
+  
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/typed.js/typed.min.js"></script>
+  <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
